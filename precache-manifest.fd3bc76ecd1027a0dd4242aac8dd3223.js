5befc1c81fff5ebefa24ce141a3083078c97f613
mohammed mustafa <mohammed.alkhddar@gmail.com>
2020-02-15 00:00:53 -0600

    Updates

---
+++ precache-manifest.fd3bc76ecd1027a0dd4242aac8dd3223.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef656a9c641b0c4048cbbd02e5b79f0",
+    "url": "/Simpe-React-App/index.html"
+  },
+  {
+    "revision": "fb91ea8aeb861c052410",
+    "url": "/Simpe-React-App/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "289c7bc4316af194b460",
+    "url": "/Simpe-React-App/static/css/main.ae4c209b.chunk.css"
+  },
+  {
+    "revision": "fb91ea8aeb861c052410",
+    "url": "/Simpe-React-App/static/js/2.87cb75fa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Simpe-React-App/static/js/2.87cb75fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "289c7bc4316af194b460",
+    "url": "/Simpe-React-App/static/js/main.6934ebb0.chunk.js"
+  },
+  {
+    "revision": "ad84b9eab7e8592eda1b",
+    "url": "/Simpe-React-App/static/js/runtime-main.6ce94e23.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Simpe-React-App/static/media/SEGA.12a3db8b.woff"
+  }
+]);
